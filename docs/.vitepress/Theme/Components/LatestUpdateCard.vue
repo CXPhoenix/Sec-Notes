3bdd1205c9394e69baca67e9db5e7271ba950a20
CXPhoenix <jate9229@gmail.com>
2024-02-04 00:10:44 +0800

    ✨ feat(Theme): Test Extend

---
+++ docs/.vitepress/Theme/Components/LatestUpdateCard.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div class="text-blue-600">Hello</div>
+</template>
---
+++ docs/.vitepress/Theme/index.js
@@ -0,0 +1,8 @@
+import DefaultTheme from "vitepress/theme";
+import LatestUpdateCard from "@components/LatestUpdateCard.vue";
+import ExtendLayouts from "./ExtendLayouts.vue";
+import "@style";
+
+export default {
+  extends: DefaultTheme,
+};
