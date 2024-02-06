c6de667a1385266f14ee6f95d14b534a784d137b
CXPhoenix <jate9229@gmail.com>
2024-02-07 01:33:24 +0800

    :wrench: 重新調整 theme
    刪除掉不必要的內容

--- docs/.vitepress/Theme/index.js
+++ docs/.vitepress/Theme/index.js
@@ -1,6 +1,4 @@
 import DefaultTheme from "vitepress/theme";
-import LatestUpdateCard from "@components/LatestUpdateCard.vue";
-import ExtendLayouts from "./ExtendLayouts.vue";
 import "@style";
 
 export default {
